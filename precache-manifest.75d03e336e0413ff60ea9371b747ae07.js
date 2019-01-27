27c22d36b4f1c124fd8057737ef71ae7cdb2bdab
Archaeologist03 <omniiknight@hotmail.com>
2019-01-27 23:46:12 +0100

    Updates

---
+++ precache-manifest.75d03e336e0413ff60ea9371b747ae07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f678c371e00b395d540bdfe9ee1964bd",
+    "url": "/fcc-quote-machine/static/media/bookshelf.f678c371.jpg"
+  },
+  {
+    "revision": "421eb8fcc56cf1be16e5",
+    "url": "/fcc-quote-machine/static/js/runtime~main.421eb8fc.js"
+  },
+  {
+    "revision": "52c485fa1ac719751c9c",
+    "url": "/fcc-quote-machine/static/js/main.52c485fa.chunk.js"
+  },
+  {
+    "revision": "47b5e4d32880c8f0090f",
+    "url": "/fcc-quote-machine/static/js/1.47b5e4d3.chunk.js"
+  },
+  {
+    "revision": "52c485fa1ac719751c9c",
+    "url": "/fcc-quote-machine/static/css/main.701decaa.chunk.css"
+  },
+  {
+    "revision": "88c81791bd823240daa9db145caa70ed",
+    "url": "/fcc-quote-machine/index.html"
+  }
+];
